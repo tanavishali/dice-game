9b6ba73f5595999d0114a257bea10f1d21bbda57
tanavishali <tanawishalrai5271@gmail.com>
2024-07-18 12:54:44 +0500

    create files

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let score1 = document.querySelector('#score--0');
+
+let score2 = document.querySelector('#score--1');
+let dicehidden = document.querySelector('.dice');
+score1.textContent = 0;
+score2.textContent = 0;
+dicehidden.classList.add('hidden');
